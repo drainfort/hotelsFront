9ad5fa1fa6376b0a5c75b11d2a83ee0c42e3e41d
Jaime <jaime.romero @skaphe.com>
2018-08-18 17:43:19 -0500

    Complteing interface

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import HotelServiceImpl from '../../services/hotel/hotel.service';
 import { HotelService } from '../../services/hotel/hotel.service.interface';
 import Filter from '../../models/filter.model';
@@ -7,20 +7,81 @@ import Hotel from '../../models/hotel.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'hotelsFront';
+  hotels: Array<Hotel> = [];
+  isOpen: Boolean = true;
+  nameFilter: Filter<string>;
+  starFilter: Filter<string>;
+  activeCombos: Array<Boolean> = [];
+  starArray: Array<number> = [0,5,4,3,2,1];
 
   constructor(@Inject(HotelServiceImpl) private hotelService: HotelService){
-    hotelService.getAll([])
-    .subscribe(
-      (agencyResponse: Array<Hotel>) => {
-        console.log("", agencyResponse)
-      },
-      () => {
-        
+    this.nameFilter = new Filter<string>();
+    this.nameFilter.name = "name";
+    this.nameFilter.value = "";
+    this.starFilter = new Filter<string>();
+    this.starFilter.name = "stars";
+    this.starFilter.value = "";
+    for(let i = 0; i<6;i++){
+      this.activeCombos.push(false);
+    }
+    this.activeCombos[0]=true;
+  }
+
+  ngOnInit() {
+    this.getAll([]);
+  }
+
+  close() {
+    this.isOpen = !this.isOpen;
+  }
+
+  arrayOne(n: number): any[] {
+    return Array(n);
+  }
+
+  searchByName() {
+    let array = [];
+    if(this.nameFilter.value!=""){
+      array.push(this.nameFilter);
+    }
+    this.getAll(array);
+  }
+
+  getAll(filters: Array<Filter<any>>){
+    this.hotelService.getAll(filters)
+      .subscribe(
+        (hotelsResponse: Array<Hotel>) => {
+          this.hotels = hotelsResponse;
+        },
+        () => {
+
+        }
+      );
+  }
+
+  checkStars(stars: number){
+    let array = [];
+    if(stars==0){
+      for(let i = 1; i<6;i++){
+        this.activeCombos[i] = false;
       }
-    );
+      this.starFilter.value =  "";
+      this.activeCombos[0] = true;
+    }
+    else{
+      this.activeCombos[0] = false;
+      let value = "";
+      for(let i = 1; i<6;i++){
+        if(this.activeCombos[i])
+          value+= this.starArray[i]+","; 
+      }
+      this.starFilter.value =value;
+      array.push(this.starFilter);
+    }
+    this.getAll(array);
   }
 }
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -4,14 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from '../components/app/app.component';
 import HotelServiceImpl from '../services/hotel/hotel.service';
 import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
+import { HeaderComponent } from '../components/header/header.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    FormsModule
   ],
   providers: [
     HttpClient,
